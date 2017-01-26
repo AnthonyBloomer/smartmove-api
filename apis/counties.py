a14fa86ee658f59f2b467a9b1e3dd44d66828576
Anthony Bloomer <ant0@protonmail.ch>
2017-01-26 15:06:04 +0000

    Added more API methods for counties

--- apis/counties.py
+++ apis/counties.py
@@ -4,10 +4,18 @@ from core.connection import conn
 api = Namespace('counties', description='Get property sale statistics for each county')
 
 county = api.model('County', {
+    'id': fields.String(required=True, description='The county identifier'),
+    'county_name': fields.String(required=True, description='The county.'),
+    'average_sale_price': fields.String(description='The average sale price.'),
+    'total_number_of_sales': fields.String(description="The total number of sales.")
+})
+
+year_stats = api.model('Year', {
+    'id': fields.String(required=True, description='The county identifier'),
     'county_name': fields.String(required=True, description='The county.'),
     'average_sale_price': fields.String(description='The average sale price.'),
     'total_number_of_sales': fields.String(description="The total number of sales."),
-    'year': fields.String(description='Year')
+    'year': fields.String(description="Year")
 })
 
 
@@ -20,24 +28,35 @@ class PropertyList(Resource):
         with conn.cursor() as cursor:
             cursor.execute(sql)
         data = cursor.fetchall()
-        return data
+        return data if data else api.abort(404)
 
 
-@api.route('/<county_name>')
-@api.param('county_name', 'The county name.')
+@api.route('/<id>')
+@api.param('id', 'The property identifier')
 @api.response(404, 'Property not found')
 class Property(Resource):
     @api.doc('get_property')
     @api.marshal_with(county)
-    def get(self, county_name):
-        # Get the overall sale statistics
-        sql = "select * from fact_county as f join dim_county as c on f.county_id = c.id where county_name like %s"
+    def get(self, id):
+        sql = "select * from fact_county as f join dim_county as c on f.county_id = c.id where f.id = %s"
         with conn.cursor() as cursor:
-            cursor.execute(sql, county_name)
+            cursor.execute(sql, id)
         data = cursor.fetchone()
+        return data if data else api.abort(404)
+
+
+@api.route('/<county_name>/<year>')
+@api.param('county_name', 'The county name.')
+@api.param('year', 'The year.')
+@api.response(404, 'Property not found')
+class Property(Resource):
+    @api.doc('get_property')
+    @api.marshal_with(year_stats)
+    def get(self, county_name, year):
         # Get yearly sale statistics for the given county.
-        sql = "select * from fact_year as f join dim_county as d on d.id = f.county_id where county_name LIKE %s"
+        sql = "select * from fact_year as f " \
+              "join dim_county as d on d.id = f.county_id where county_name like %s and year = %s "
         with conn.cursor() as cursor:
-            cursor.execute(sql, county_name)
+            cursor.execute(sql, [county_name, year])
         y = cursor.fetchall()
-        return [data, y] if data and y else api.abort(404)
+        return y if y else api.abort(404)
