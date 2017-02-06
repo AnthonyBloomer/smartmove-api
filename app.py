561d733b1a0c90621a438f4265320eaa76cfd18c
Anthony Bloomer <ant0@protonmail.ch>
2017-02-06 19:33:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from apis import api
+import os 
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
@@ -12,4 +13,6 @@ limiter = Limiter(
     global_limits=["2000 per day", "100 per hour"]
 )
 
-app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 33507)) 
+    app.run(debug=True, host='0.0.0.0', port=port)
